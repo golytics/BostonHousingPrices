856c11ca3f794e8dcf873f477f522e631752f335
gbrbreen1 <gbrbreen2@gmail.com>
2021-11-03 21:12:39 +0400

    initial commit

---
+++ boston-housing-ml.py
@@ -0,0 +1,141 @@
+import streamlit as st
+import pandas as pd
+import shap
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from sklearn.ensemble import RandomForestRegressor
+
+
+
+# configuring the page and the logo
+st.set_page_config(page_title='Mohamed Gabr - House Price Prediction', page_icon ='logo.png', layout = 'wide', initial_sidebar_state = 'auto')
+
+
+import os
+import base64
+
+# the functions to prepare the image to be a hyperlink
+@st.cache(allow_output_mutation=True)
+def get_base64_of_bin_file(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+@st.cache(allow_output_mutation=True)
+def get_img_with_href(local_img_path, target_url):
+    img_format = os.path.splitext(local_img_path)[-1].replace('.', '')
+    bin_str = get_base64_of_bin_file(local_img_path)
+    html_code = f'''
+        <a href="{target_url}">
+            <img src="data:image/{img_format};base64,{bin_str}" />
+        </a>'''
+    return html_code
+
+
+# preparing the layout for the top section of the app
+# dividing the layout vertically (dividing the first row)
+row1_1, row1_2, row1_3 = st.columns((1, 4, 5))
+
+# first row first column
+with row1_1:
+    gif_html = get_img_with_href('logo.png', 'https://golytics.github.io/')
+    st.markdown(gif_html, unsafe_allow_html=True)
+
+with row1_2:
+    # st.image('logo.png')
+    st.title("Predicting House Prices in Bangalore, India")
+    st.markdown("<h2>A POC for a Real Estate Client</h2>", unsafe_allow_html=True)
+
+# first row second column
+with row1_3:
+    st.info(
+        """
+        ##
+        This data product has been prepared as a proof of concept of a machine learning model to predict prices of houses in Cairo, Egypt.
+        For demonstration purposes, we have used the data from Bangalore to prove the technical feasibility of the model. Developing the final model required
+        many steps following the CRISP-DM methodology. After building the model we used it to predict the prices in this application. **The model can be changed/
+        enhanced for any another city based on its own data.**
+        """)
+st.write("""
+# Boston House Price Prediction App
+
+This app predicts the **Boston House Price**!
+""")
+st.write('---')
+
+# Loads the Boston House Price Dataset
+boston = datasets.load_boston()
+X = pd.DataFrame(boston.data, columns=boston.feature_names)
+Y = pd.DataFrame(boston.target, columns=["MEDV"])
+
+# Sidebar
+# Header of Specify Input Parameters
+st.sidebar.header('Specify Input Parameters')
+
+def user_input_features():
+    CRIM = st.sidebar.slider('CRIM', X.CRIM.min(), X.CRIM.max(), X.CRIM.mean())
+    ZN = st.sidebar.slider('ZN', X.ZN.min(), X.ZN.max(), X.ZN.mean())
+    INDUS = st.sidebar.slider('INDUS', X.INDUS.min(), X.INDUS.max(), X.INDUS.mean())
+    CHAS = st.sidebar.slider('CHAS', X.CHAS.min(), X.CHAS.max(), X.CHAS.mean())
+    NOX = st.sidebar.slider('NOX', X.NOX.min(), X.NOX.max(), X.NOX.mean())
+    RM = st.sidebar.slider('RM', X.RM.min(), X.RM.max(), X.RM.mean())
+    AGE = st.sidebar.slider('AGE', X.AGE.min(), X.AGE.max(), X.AGE.mean())
+    DIS = st.sidebar.slider('DIS', X.DIS.min(), X.DIS.max(), X.DIS.mean())
+    RAD = st.sidebar.slider('RAD', X.RAD.min(), X.RAD.max(), X.RAD.mean())
+    TAX = st.sidebar.slider('TAX', X.TAX.min(), X.TAX.max(), X.TAX.mean())
+    PTRATIO = st.sidebar.slider('PTRATIO', X.PTRATIO.min(), X.PTRATIO.max(), X.PTRATIO.mean())
+    B = st.sidebar.slider('B', X.B.min(), X.B.max(), X.B.mean())
+    LSTAT = st.sidebar.slider('LSTAT', X.LSTAT.min(), X.LSTAT.max(), X.LSTAT.mean())
+    data = {'CRIM': CRIM,
+            'ZN': ZN,
+            'INDUS': INDUS,
+            'CHAS': CHAS,
+            'NOX': NOX,
+            'RM': RM,
+            'AGE': AGE,
+            'DIS': DIS,
+            'RAD': RAD,
+            'TAX': TAX,
+            'PTRATIO': PTRATIO,
+            'B': B,
+            'LSTAT': LSTAT}
+    features = pd.DataFrame(data, index=[0])
+    return features
+
+df = user_input_features()
+
+# Main Panel
+
+# Print specified input parameters
+st.header('Specified Input parameters')
+st.write(df)
+st.write('---')
+
+# Build Regression Model
+model = RandomForestRegressor()
+model.fit(X, Y)
+# Apply Model to Make Prediction
+prediction = model.predict(df)
+
+st.header('Prediction of MEDV')
+st.write(prediction)
+st.write('---')
+
+# Explaining the model's predictions using SHAP values
+# https://github.com/slundberg/shap
+explainer = shap.TreeExplainer(model)
+shap_values = explainer.shap_values(X)
+
+st.header('Feature Importance')
+plt.title('Feature importance based on SHAP values')
+shap.summary_plot(shap_values, X)
+st.pyplot(bbox_inches='tight')
+st.write('---')
+
+plt.title('Feature importance based on SHAP values (Bar)')
+shap.summary_plot(shap_values, X, plot_type="bar")
+st.pyplot(bbox_inches='tight')
+
+
+with open("style.css") as f:
+    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
